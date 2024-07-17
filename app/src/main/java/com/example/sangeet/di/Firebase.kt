c2aecbb63d93a7a81c5241f74657c3a35b44c643
Ayush Thakur <ayushthakur00@gmail.com>
2024-07-17 22:37:54 +0530

    set up dependency injection for firebase common references

---
+++ app/src/main/java/com/example/sangeet/di/Firebase.kt
@@ -0,0 +1,45 @@
+package com.example.sangeet.di
+
+import android.content.Context
+import com.example.sangeet.repositry.repo
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object Firebase {
+
+    @Provides
+    fun provideFirebaseApp(@ApplicationContext context: Context): FirebaseApp {
+        return FirebaseApp.initializeApp(context)!!
+    }
+
+        @Provides
+    fun provideFirebaseAuth(firebaseApp: FirebaseApp): FirebaseAuth {
+        return FirebaseAuth.getInstance(firebaseApp)
+    }
+
+    @Provides
+    fun provideFirebaseStorage(firebaseApp: FirebaseApp): FirebaseStorage {
+        return FirebaseStorage.getInstance(firebaseApp)
+    }
+
+    @Provides
+    fun provideDatabase(): FirebaseFirestore {
+        return Firebase.firestore
+    }
+
+    @Provides
+    fun provideRepo(firebaseStorage: FirebaseStorage, db: FirebaseFirestore): repo {
+        return repo(firebaseStorage, db)
+    }
+}
